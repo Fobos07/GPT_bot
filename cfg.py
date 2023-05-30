3d0d8f609720b8abf4dc3498b7bd0175340c5f12
Fobos07 <52098297+Fobos07@users.noreply.github.com>
2023-05-30 14:07:21 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,13 +1,12 @@
-bot_token = '6091338198:AAFmYIoFFBq_BWOVefgQOLszHrw5Z8o5X7c'
-# bot_token = '6298011214:AAFEczYvjYu3531A5ULkTW0pSP2JTCHF_Ik'
-ai_key = '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'
+bot_token = ''
+ai_key = ''
 pay_token_test = '390540012:LIVE:34048'
 
 db_config = {
-    'host': '185.182.110.172', 
-    'user': 'admin',
-    'password': 'Qz123_xX45',
-    'db_name': 'GPT_BOT'
+    'host': '', 
+    'user': '',
+    'password': '',
+    'db_name': ''
     }
 
 commands = ['/start', '/profile', '/buy', '/info', '/settings']
